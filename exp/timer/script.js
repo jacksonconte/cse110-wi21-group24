cb911c8b23b6c7cb87b6532cfa12bcca02b955a9
Anurag Asthana <anurag.a.asthana@gmail.com>
2021-02-13 20:45:21 -0800

    Lots of testing and changing configs

--- exp/timer/script.js
+++ exp/timer/script.js
@@ -2,26 +2,26 @@ let timerStarted = false;
 let timerSeconds, intervalID, button, readout;
 const DURATION = 60; // seconds for timer
 
-function startTimer() {
+function startTimer () {
   intervalID = setInterval(tick, 1000);
-  button.innerHTML = "Stop";
+  button.innerHTML = 'Stop';
   timerStarted = true;
 }
-function stopTimer() {
+function stopTimer () {
   clearInterval(intervalID);
-  button.innerHTML = "Start";
+  button.innerHTML = 'Start';
   timerStarted = false;
   timerSeconds = DURATION;
   readout.innerHTML = timerSeconds;
 }
-function tick() {
+function tick () {
   readout.innerHTML = --timerSeconds;
-  if (timerSeconds == 0) stopTimer();
+  if (timerSeconds === 0) stopTimer();
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  button = document.getElementById("toggle");
-  readout = document.getElementById("readout");
+window.addEventListener('DOMContentLoaded', () => {
+  button = document.getElementById('toggle');
+  readout = document.getElementById('readout');
   button.onclick = () => {
     if (timerStarted) {
       stopTimer();
--- test.js
+++ test.js
@@ -34,3 +34,5 @@ console.log(subNum(4, 5));
 function multNum(num1, num2) {
   return num1 * num2;
 }
+
+console.log(multNum(4, 5));
