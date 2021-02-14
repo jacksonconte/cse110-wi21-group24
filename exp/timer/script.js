a136fc97de01e95c4d409d20e45edd5a6564ff5f
Roger Ji <roger.ji.32021@gmail.com>
2021-02-13 21:07:20 -0800

    basic timer animation, but text inside is flipped

--- exp/timer/script.js
+++ exp/timer/script.js
@@ -1,3 +1,4 @@
+/*
 let timerStarted = false;
 let timerSeconds, intervalID, button, readout;
 const DURATION = 60; // seconds for timer
@@ -22,6 +23,32 @@ function tick() {
 window.addEventListener('DOMContentLoaded', () => {
     button = document.getElementById('toggle');
     readout = document.getElementById('readout');
+    button.onclick = () => {
+        if (timerStarted) {
+            stopTimer();
+        } else {
+            startTimer();
+        }
+        timerSeconds = DURATION;
+    };
+});
+*/
+
+window.addEventListener('DOMContentLoaded', () => {
+    button = document.getElementById('toggle');
+    readout = document.getElementById('readout');
+    
+    var countdownNumberEl = document.getElementById('countdown-number');
+    var countdown = 10;
+
+    countdownNumberEl.textContent = countdown;
+
+    setInterval(function () {
+        countdown = --countdown <= 0 ? 10 : countdown;
+
+        countdownNumberEl.textContent = countdown;
+    }, 1000);
+
     button.onclick = () => {
         if (timerStarted) {
             stopTimer();
