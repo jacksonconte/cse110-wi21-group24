f4d1532a7524b4de7c078c4ba74c5d1bd6eb61c7
Anurag Asthana <anurag.a.asthana@gmail.com>
2021-02-13 16:11:58 -0800

    More testing of documentation

--- test.js
+++ test.js
@@ -1,10 +1,24 @@
 
+/**
+ * Adds two numbers together
+ * @method
+ * @param {Number} num1
+ * @param {Number} num2
+ * @returns {Number} sum of the two numbers
+ */
 function addNum (num1, num2) {
   return num1 + num2
 }
 
 console.log(addNum(4, 5))
 
+/**
+ * Multiplies two numbers together
+ * @method
+ * @param {Number} num1
+ * @param {Number} num2
+ * @returns {Number} product of the two numbers
+ */
 function multNum (num1, num2) {
   return num1 * num2
 }
