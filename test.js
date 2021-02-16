b2d0e072fe4dfd06497a47ca61d99bbb933a6aba
Anurag Asthana <anurag.a.asthana@gmail.com>
2021-02-16 15:52:26 -0800

    More documentation testing

--- test.js
+++ test.js
@@ -37,6 +37,13 @@ function multNum(num1, num2) {
 
 console.log(multNum(4, 2));
 
+/**
+ * Divides two numbers
+ * @method
+ * @param {Number} num1
+ * @param {Number} num2
+ * @returns {Number} quotient of the two numbers
+ */
 function divNum(num1, num2) {
   return num1 * num2;
 }
