0553cf28946f83921cc249f1705eeafb4d335494
noahverdegan <nverdega@ucsd.edu>
2021-02-16 18:11:47 -0800

    Merged all html files into one, now sidebar just changes what is displayed on a single page.

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,62 @@ function openNav() {
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
   function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
   }
   
+  function openTimer() {
+    /* display appropriate content */
+    document.getElementById("tasks").style.display = "none";
+    document.getElementById("analytics").style.display = "none";
+    document.getElementById("settings").style.display = "none";
+    document.getElementById("timer").style.display = "block";
+
+    /* update buttons */
+    document.getElementById("timerbtn").onclick = closeNav;
+    document.getElementById("tasksbtn").onclick = openTasks;
+    document.getElementById("analyticsbtn").onclick = openAnalytics;
+    document.getElementById("settingsbtn").onclick = openSettings;
+
+    closeNav();
+  }
+
+  function openTasks() {
+    document.getElementById("timer").style.display = "none";
+    document.getElementById("analytics").style.display = "none";
+    document.getElementById("settings").style.display = "none";
+    document.getElementById("tasks").style.display = "block";
+
+    document.getElementById("timerbtn").onclick = openTimer;
+    document.getElementById("tasksbtn").onclick = closeNav;
+    document.getElementById("analyticsbtn").onclick = openAnalytics;
+    document.getElementById("settingsbtn").onclick = openSettings;
+
+    closeNav();
+  }
+
+  function openAnalytics() {
+    document.getElementById("timer").style.display = "none";
+    document.getElementById("tasks").style.display = "none";
+    document.getElementById("settings").style.display = "none";
+    document.getElementById("analytics").style.display = "block";
+
+    document.getElementById("timerbtn").onclick = openTimer;
+    document.getElementById("tasksbtn").onclick = openTasks;
+    document.getElementById("analyticsbtn").onclick = closeNav;
+    document.getElementById("settingsbtn").onclick = openSettings;
+
+    closeNav();
+  }
+
+  function openSettings() {
+    document.getElementById("timer").style.display = "none";
+    document.getElementById("tasks").style.display = "none";
+    document.getElementById("analytics").style.display = "none";
+    document.getElementById("settings").style.display = "block";
+
+    document.getElementById("timerbtn").onclick = openTimer;
+    document.getElementById("tasksbtn").onclick = openTasks;
+    document.getElementById("analyticsbtn").onclick = openAnalytics;
+    document.getElementById("settingsbtn").onclick = closeNav;
+
+    closeNav();
+  }
