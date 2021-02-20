a1711b718acf2e8a98082d3a50a0e3ffa2dbfb0d
noahverdegan <nverdega@ucsd.edu>
2021-02-20 14:49:40 -0800

    Used Anshul's code to better match figma

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,11 +1,15 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("mySidebar").style.width = "350px";
+    document.getElementById("openButton").style.paddingLeft = "275px";
+    document.getElementById("openButton").onclick = closeNav;
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
   function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("openButton").style.paddingLeft = "15px";
+    document.getElementById("openButton").onclick = openNav;
   }
   
   function openTimer() {
