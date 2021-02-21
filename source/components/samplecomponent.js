18eb989d97443ed2822db508b51567a64676bd40
davidjeong0628 <davidjeong0628@gmail.com>
2021-02-20 16:28:05 -0800

    Prepare for pull request

--- source/components/samplecomponent.js
+++ source/components/samplecomponent.js
@@ -1,37 +1,37 @@
-// /**
-//  * @class Triangle
-//  * @description A class made to hold the properties of a triangle
-//  */
-// class Triangle {
-//     /**
-//      * @constructor
-//      * @description Constructor of a Triangle object
-//      * @param {Number} base
-//      * @param {Number} height
-//      */
-//     constructor(base, height) {
-//         this.base = base;
-//         this.height = height;
-//     }
+/**
+ * @class Triangle
+ * @description A class made to hold the properties of a triangle
+ */
+class Triangle {
+    /**
+     * @constructor
+     * @description Constructor of a Triangle object
+     * @param {Number} base
+     * @param {Number} height
+     */
+    constructor(base, height) {
+        this.base = base;
+        this.height = height;
+    }
 
-//     /**
-//      * @method area
-//      * @description Calclulates the area of the Triangle
-//      * @returns {Number} The area of the Triangle
-//      */
-//     area() {
-//         return (this.base * this.height) / 2;
-//     }
+    /**
+     * @method area
+     * @description Calclulates the area of the Triangle
+     * @returns {Number} The area of the Triangle
+     */
+    area() {
+        return (this.base * this.height) / 2;
+    }
 
-//     /**
-//      * @method perimeter
-//      * @description Calculates the perimeter of a Triangle
-//      * @param {Number} side1
-//      * @param {Number} side2
-//      * @param {Number} side3
-//      * @returns {Number} The perimeter of the Triangle
-//      */
-//     perimeter(side1, side2, side3) {
-//         return side1 + side2 + side3;
-//     }
-// }
+    /**
+     * @method perimeter
+     * @description Calculates the perimeter of a Triangle
+     * @param {Number} side1
+     * @param {Number} side2
+     * @param {Number} side3
+     * @returns {Number} The perimeter of the Triangle
+     */
+    perimeter(side1, side2, side3) {
+        return side1 + side2 + side3;
+    }
+}
