124d13f3b50495cddedbbeafdb89afc49f3b888c
Anurag Asthana <anurag.a.asthana@gmail.com>
2021-03-11 21:37:30 -0800

    Fixed some linting and got Cypress working

---
+++ cypress/integration/settingstest.js
@@ -0,0 +1,9 @@
+describe("Settings test for Pomo Work Time", () => {
+    beforeEach(() => {
+        cy.visit('http://127.0.0.1:5500/source');
+    });
+
+    it("First Test", () => {
+        expect(true).to.equal(true);
+    });
+});
