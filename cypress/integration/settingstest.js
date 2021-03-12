338f9a237a86cc8392d3092922065bcefcc4b614
Anurag Asthana <anurag.a.asthana@gmail.com>
2021-03-11 22:32:44 -0800

    Implementing first Cypress tests for Settings

--- cypress/integration/settingstest.js
+++ cypress/integration/settingstest.js
@@ -1,9 +1,29 @@
-describe("Settings test for Pomo Work Time", () => {
+describe('Settings test for Pomo Work Time', () => {
     beforeEach(() => {
         cy.visit('http://127.0.0.1:5500/source');
+        cy.get('#openButton').click();
+        cy.get('#settingsbtn').click();
     });
 
-    it("First Test", () => {
-        expect(true).to.equal(true);
+    it('Check Pomo time inputs in Local Storage', () => {
+        cy.get('#work-pomo-time').clear().type('30');
+        cy.get('#short-break-time').clear().type('10');
+        cy.get('#long-break-time').clear().type('45');
+        cy.get('#submit-button').click().should(() => {
+            expect(localStorage.getItem('workPomoTime')).to.equal('30');
+            expect(localStorage.getItem('shortBreakTime')).to.equal('10');
+            expect(localStorage.getItem('longBreakTime')).to.equal('45');
+        });
+        
+        
+    });
+
+    it('Check for invalid inputs in Pomo times', () => {
+        cy.get('#work-pomo-time').clear().type('-1');
+        cy.get('input:invalid').should('have.length', 1);
+        cy.get('#short-break-time').clear().type('-1');
+        cy.get('input:invalid').should('have.length', 2);
+        cy.get('#long-break-time').clear().type('-1');
+        cy.get('input:invalid').should('have.length', 3);
     });
 });
