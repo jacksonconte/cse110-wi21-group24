dc4f3b67bdae34971ea6b6c96572759733442e5d
Jackson Conte <jacksonjconte@gmail.com>
2021-03-12 20:36:35 -0800

    test adjustment

--- cypress/integration/timertest.js
+++ cypress/integration/timertest.js
@@ -1,9 +1,9 @@
 describe('Testing the timer page', () => {
     beforeEach(() => {
         cy.visit('http://127.0.0.1:5500/source');
-        localStorage.setItem('workPomoTime', toString(1/60)); // 1 second each
-        localStorage.setItem('shortBreakTime', toString(1/60));
-        localStorage.setItem('longBreakTime', toString(1/60));
+        localStorage.setItem('workPomoTime', '0.025'); // 1.5 second each
+        localStorage.setItem('shortBreakTime', '0.025');
+        localStorage.setItem('longBreakTime', '0.025');
         cy.get('#openButton').click();
         cy.get('#tasksbtn').click();
     });
@@ -13,7 +13,7 @@ describe('Testing the timer page', () => {
         cy.get('#input-pomos').clear().type('1');
         cy.get('#add-task-button').click();
         cy.get('.start-task-btn').click();
-        setTimeout(1000, () => {
+        setTimeout(1500, () => {
             expect(localStorage.get('curr-task')).to.equal(null);
         });
     });
