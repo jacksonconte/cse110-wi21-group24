965bb5e6b31ab61fe6161a8107a571500e84b568
Jackson Conte <jacksonjconte@gmail.com>
2021-03-12 20:26:29 -0800

    added class to start task button, added timer cypress test

--- cypress/integration/timertest.js
+++ cypress/integration/timertest.js
@@ -1,7 +1,20 @@
 describe('Testing the timer page', () => {
     beforeEach(() => {
         cy.visit('http://127.0.0.1:5500/source');
+        localStorage.setItem('workPomoTime', toString(1/60)); // 1 second each
+        localStorage.setItem('shortBreakTime', toString(1/60));
+        localStorage.setItem('longBreakTime', toString(1/60));
         cy.get('#openButton').click();
         cy.get('#tasksbtn').click();
+    });
+
+    it('Task starts and finishes', () => {
+        cy.get('#input-task-name').clear().type('Test Task');
+        cy.get('#input-pomos').clear().type('1');
+        cy.get('#add-task-button').click();
+        cy.get('.start-task-btn').click();
+        setTimeout(1000, () => {
+            expect(localStorage.get('curr-task')).to.equal(null);
+        });
     });
 });
