0a5075e25d7b8a43a1f450916b4994380ae01e4a
Anurag Asthana <anurag.a.asthana@gmail.com>
2021-03-12 16:51:45 -0800

    Fixed description error in settings test file

--- cypress/integration/settingstest.js
+++ cypress/integration/settingstest.js
@@ -1,4 +1,4 @@
-describe('Testin the Settings Page', () => {
+describe('Testing the Settings Page', () => {
     beforeEach(() => {
         cy.visit('http://127.0.0.1:5500/source');
         cy.get('#openButton').click();
